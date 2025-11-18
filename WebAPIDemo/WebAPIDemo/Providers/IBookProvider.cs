9f8b76cafccd09cf30c8586d2805bfb1a57c6602
adamkulik <hamstapl@gmail.com>
2025-11-18 18:15:38 +0100

    Added book models and providers
    Added book models and providers to the project

---
+++ WebAPIDemo/WebAPIDemo/Providers/IBookProvider.cs
@@ -0,0 +1,16 @@
+﻿using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Providers
+{
+    public interface IBookProvider
+    {
+        Task<IEnumerable<Book>> GetAllBooksAsync();
+        Task<Book?> GetBookByIdAsync(int id);
+        Task<IEnumerable<Book>> GetBooksByAuthorAsync(string author);
+        Task<IEnumerable<Book>> GetBooksByGenreAsync(string genre);
+        Task<Book> AddBookAsync(Book book);
+        Task<Book?> UpdateBookAsync(Book book);
+        Task<bool> DeleteBookAsync(int id);
+
+    }
+}
---
+++ WebAPIDemo/WebAPIDemo/Providers/SampleBookProvider.cs
@@ -0,0 +1,75 @@
+﻿using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Providers
+{
+    public class SampleBookProvider : BookProvider
+    {
+        public SampleBookProvider() : base(GenerateSampleBooks())
+        {
+
+        }
+
+
+        private static List<Book> GenerateSampleBooks()
+        {
+            return new List<Book>
+            {
+                new Book
+                {
+                    Id = 1,
+                    Title = "The Great Gatsby",
+                    Author = "F. Scott Fitzgerald",
+                    Genre = "Classic"
+                },
+                new Book
+                {
+                    Id = 2,
+                    Title = "To Kill a Mockingbird",
+                    Author = "Harper Lee",
+                    Genre = "Fiction"
+                },
+                new Book
+                {
+                    Id = 3,
+                    Title = "1984",
+                    Author = "George Orwell",
+                    Genre = "Dystopian"
+                },
+                new Book
+                {
+                    Id = 4,
+                    Title = "Pride and Prejudice",
+                    Author = "Jane Austen",
+                    Genre = "Romance"
+                },
+                new Book
+                {
+                    Id = 5,
+                    Title = "The Hobbit",
+                    Author = "J.R.R. Tolkien",
+                    Genre = "Fantasy"
+                },
+                new Book
+                {
+                    Id = 6,
+                    Title = "The Shining",
+                    Author = "Stephen King",
+                    Genre = "Horror"
+                },
+                new Book
+                {
+                    Id = 7,
+                    Title = "Dune",
+                    Author = "Frank Herbert",
+                    Genre = "Science Fiction"
+                },
+                new Book
+                {
+                    Id = 8,
+                    Title = "Murder on the Orient Express",
+                    Author = "Agatha Christie",
+                    Genre = "Mystery"
+                }
+            };
+        }
+}
